5e03def8c4e50298027b1f56d8ad6c34c5dd0fe6
Kchaou chedly <82588422+chedlykch@users.noreply.github.com>
2021-05-19 00:57:41 +0200

    files of last commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# ad-mania-
-changement des remarques sur le back + correction 
-et front relative au module emplacements + 3 métiers 
--- modifiermapc.php
+++ modifiermapc.php
@@ -5,10 +5,10 @@ include '../../core/mapc.php';
 
 
 
-if( isset($_POST['type']) and isset($_POST['nom']) and isset($_POST['photo']) )
+if( isset($_POST['type']) and isset($_POST['nom']) and isset($_POST['image']) )
 {
 
-$map = new map($_POST['type'],$_POST['nom'],$_POST['photo']);
+$map = new map($_POST['type'],$_POST['nom'],$_POST['image']);
 	
 
 	$mapc=new mapc();
